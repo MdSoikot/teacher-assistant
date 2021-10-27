0e7eb74cbd7e652e1e5205f04e449d4a9f0b091e
litonshil <litonshil009@gmail.com>
2021-10-28 02:25:07 +0600

    tailwindcss add and folder structure improved

---
+++ resources/js/Shared/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='flex'>
+            <div>
+                Teaching Assist
+            </div>
+            <div>
+                <span>About</span>
+                <span>Sign In</span>
+                <span>Sign Up</span>
+                <span>Logout</span>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
